94dba1f8da578d8a0bc6c68123d4e19576ce3d84
tomohiro.maeno <>
2024-10-13 23:12:46 +0700

    add: Add a practice file for argument default values in ES2015.

---
+++ tsc/practice.js
@@ -0,0 +1,6 @@
+"use strict";
+const sum = (a, b) => {
+    return a * b;
+};
+let answer = sum('あいう', 2);
+console.log(answer);
---
+++ tsc/practice.ts
@@ -0,0 +1,6 @@
+const sum = (a: number, b: number): number => {
+  return a * b;
+}
+
+let answer: number = sum('あいう', 2);
+console.log(answer);
