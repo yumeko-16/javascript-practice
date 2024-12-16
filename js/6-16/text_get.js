c694a0a77b4f5fa9efe56434e84ca3df87f87064
tomohiro.maeno <>
2024-12-16 17:49:41 +0700

    add: Add practice files for `textContent` / `innerHTML` properties.

---
+++ js/6-16/text_get.js
@@ -0,0 +1,9 @@
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    const list = document.getElementById('list');
+    console.log(list.innerHTML);
+    console.log(list.textContent);
+  },
+  false,
+);
