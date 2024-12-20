9d1ee7ef5aebc20774ee30a78dc2020beba7159d
tomohiro.maeno <>
2024-12-20 23:57:26 +0700

    change: Modify the filter/map code.

--- js/6-23/list.js
+++ js/6-23/list.js
@@ -6,13 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const opts = document.getElementById(name).options;
     const optsArray = Array.from(opts);
 
-    for (const opt of optsArray) {
-      if (opt.selected) {
-        result.push(opt.value);
-      }
-    }
+    // for (const opt of optsArray) {
+    //   if (opt.selected) {
+    //     result.push(opt.value);
+    //   }
+    // }
 
-    return result;
+    // return result;
+
+    return Array.from(opts)
+      .filter((opt) => opt.selected)
+      .map((opt) => opt.value);
   };
 
   btn.addEventListener('click', () => {
