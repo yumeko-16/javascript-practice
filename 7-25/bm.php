7aa29cc893fef3cf126e4b89d2b7717c4d47ef59
tomohiro.maeno <>
2025-01-14 00:04:11 +0700

    change: Prettier ni PHP no settei o tsuika suru. Add PHP settings to Prettier.

--- 7-25/bm.php
+++ 7-25/bm.php
@@ -7,4 +7,9 @@ header('Content-Type: application/json;charset=UTF-8');
 // はてブAPIへの問い合わせURLを組み立て
 $url = 'http://b.hatena.ne.jp/entry/jsonlite/?url=' . $_GET['url'];
 // 問い合わせ結果をそのまま出力
-print(file_get_contents($url, false, stream_context_create(['http' => ['header' => 'User-Agent: MySample']])));
+print file_get_contents(
+    $url,
+    false,
+    stream_context_create(['http' => ['header' => 'User-Agent: MySample']]),
+);
+?>
