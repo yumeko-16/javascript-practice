9d42329345ee9550b72a9525c242a3e63893bb3c
tomohiro.maeno <>
2025-06-18 20:05:03 +0700

    add: Multiple asynchronous processes are executed in parallel.

---
+++ dist/assets/js/7-34/promise_all.js
@@ -0,0 +1,10 @@
+import { AsyncProcess } from '../7-32/asyncProcess.js';
+Promise.all([
+    AsyncProcess('トクジロウ'),
+    AsyncProcess('ニンザブロウ'),
+    AsyncProcess('リンリン'),
+]).then((response) => {
+    console.log(response);
+}, (error) => {
+    console.log(`エラー：${error}`);
+});
