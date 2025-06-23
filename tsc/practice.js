73f982759ca2a92b2399a192cc9d05aa11c808bd
tomohiro.maeno <>
2025-06-23 19:51:25 +0700

    remove: Delete unneeded file sets.

--- tsc/practice.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const sum = (a, b) => {
-    return a * b;
-};
-let answer = sum('あいう', 2);
-console.log(answer);
--- tsc/practice.ts
+++
@@ -1,6 +0,0 @@
-const sum = (a: number, b: number): number => {
-  return a * b;
-}
-
-let answer: number = sum('あいう', 2);
-console.log(answer);
